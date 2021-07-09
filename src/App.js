6fee0226dfc2613009571ad8a688eea2b807f972
Biswajit-sahu <54319031+Biswajit-sahu@users.noreply.github.com>
2021-07-09 20:54:07 +0530

    Add files via upload

---
+++ src/App.js
@@ -0,0 +1,55 @@
+import React, { useState } from "react";
+import "./styles.css";
+
+var emojiDictionary = {
+  "🤔": "Thinking Face",
+  "😉": "Winking Face",
+  "😍": "Heart Eyes and Smiling Face",
+  "🤣": "Rolling on the floor laughing",
+  "🤑": "Money Mouth Face",
+  "🤫": "Shushing Face"
+};
+
+var emojisWeKnow = Object.keys(emojiDictionary);
+
+export default function App() {
+  var [meaning, setMeaning] = useState("");
+
+  function emojiInputHandler(event) {
+    var userInput = event.target.value;
+
+    var meaning = emojiDictionary[userInput];
+    if (meaning === undefined) {
+      meaning = "We don't have this emoji in our database";
+    }
+    setMeaning(meaning);
+  }
+
+  function emojiClickHandler(emoji) {
+    var meaning = emojiDictionary[emoji];
+    setMeaning(meaning);
+  }
+  return (
+    <div className="App">
+      <h1 style={{ color: "blueviolet" }}>Welcome to Emoji Interpreter!!!</h1>
+      <h2>Search any Emoji to know about it</h2>
+      <input
+        onChange={emojiInputHandler}
+        placeholder="Search any emoji here"
+      ></input>
+      <h3>{meaning}</h3>
+      <h2>Emojis we know 👇</h2>
+      {emojisWeKnow.map(function (emoji) {
+        return (
+          <span
+            id="emojis"
+            key={emoji}
+            onClick={() => emojiClickHandler(emoji)}
+          >
+            {emoji}
+          </span>
+        );
+      })}
+    </div>
+  );
+}
